b243e9992b6dfefffab119344b64b61b79e93eb3
Louis Law <louiscklaw@users.noreply.github.com>
2022-07-19 15:34:49 +0800

    update bull-provision-helloworld,

--- bull/bull-provision-helloworld/server.js
+++ bull/bull-provision-helloworld/server.js
@@ -3,7 +3,6 @@ const Queue = require('bull');
 
 const { createBullBoard } = require('@bull-board/api');
 const { BullAdapter } = require('@bull-board/api/bullAdapter');
-const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
 const { ExpressAdapter } = require('@bull-board/express');
 
 const lineQueue = require('./lib/queues/lineQueue');
@@ -11,6 +10,9 @@ const anotherQueue = require('./lib/queues/anotherQueue');
 
 const initReturnValueQueue = require('./queues/returnValueQueue');
 const returnValueQueue = new Queue('returnValueQueue', 'redis://localhost:6380');
+
+const initDockerOperationQueue = require('./queues/dockerOperationQueue');
+const dockerOperationQueue = new Queue('dockerOperationQueue', 'redis://localhost:6380');
 
 const routes = require('./routes');
 
@@ -21,15 +23,19 @@ const serverAdapter = new ExpressAdapter({ basePath: '/admin/queues' });
 
 serverAdapter.setBasePath('/admin/queues');
 const { addQueue, removeQueue, setQueues, replaceQueues } = createBullBoard({
-  queues: [new BullAdapter(lineQueue), new BullAdapter(anotherQueue), new BullAdapter(returnValueQueue)],
+  queues: [new BullAdapter(lineQueue), new BullAdapter(anotherQueue), new BullAdapter(returnValueQueue), new BullAdapter(dockerOperationQueue)],
   serverAdapter: serverAdapter,
 });
 
 app.use('/admin/queues', serverAdapter.getRouter());
 // bull-board setup
 
+// helloworld
 routes(app);
+
+// production queue
 initReturnValueQueue(app, returnValueQueue);
+initDockerOperationQueue(app, dockerOperationQueue);
 
 app.listen(3000, () => {
   console.log('Running on 3000...');
