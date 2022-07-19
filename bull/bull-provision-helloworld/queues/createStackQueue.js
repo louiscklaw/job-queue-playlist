c37242eb3c23750505c6e40f6245ed279b86c610
Louis Law <louiscklaw@users.noreply.github.com>
2022-07-19 19:33:04 +0800

    update bull-provision-helloworld,

--- bull/bull-provision-helloworld/queues/createStackQueue.js
+++ bull/bull-provision-helloworld/queues/createStackQueue.js
@@ -5,17 +5,20 @@ module.exports = (app, queue) => {
     var { name } = job.data;
     (async () => {
       await fetch(`http://localhost:3000/dockerOperationQueue?oper=create&stack_name=${name}`);
+
       done(null, { return: 'value return ?' });
     })();
   });
 
   app.get('/createStackQueue', (req, res) => {
-    if (!req.query.name) {
-      res.send({ status: 'error', message: 'name is required' });
-      return;
-    }
+    (async () => {
+      if (!req.query.name) {
+        res.send({ status: 'error', message: 'name is required' });
+        return;
+      }
 
-    queue.add({ name: req.query.name }, { attempts: 1 });
-    res.send({ status: 'accepted' });
+      queue.add({ name: req.query.name }, { attempts: 1 });
+      res.send({ status: 'accepted' });
+    })();
   });
 };
